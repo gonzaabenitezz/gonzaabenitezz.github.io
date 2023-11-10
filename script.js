3331d3ea0c4e0334c9aa65d33aea35d0a920d93c
gonzaabenitezz <111592792+gonzaabenitezz@users.noreply.github.com>
2023-11-10 14:05:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let loadMoreBtn1 = document.querySelector('#load-more-1');
-let currentItem1 = 4;
+let loadMoreBtn1 = document.querySelector('#load-more-1'); /* se selecciona un botón con el id #load-more-1 y se asigna a la variable loadMoreBtn1. */
+let currentItem1 = 4;   /* se inicializa currentItem1 con el valor 4 */
 
-loadMoreBtn1.onclick = () => {
+loadMoreBtn1.onclick = () => {  /* cuando el boton loadMoreBtn1 recibe un click se ejecutara lo que esta abajo */
     let boxes = [...document.querySelectorAll(
-        '.box-container-1 .box-1'
+        '.box-container-1 .box-1'   /* Se seleccionan todos los elementos con la clase .box-1 que están dentro de un contenedor con la clase .box-container-1. Se almacenan en un array llamado boxes */
     )];
 
     for(var i = currentItem1; i < currentItem1 + 4; i++){
-        boxes[i].style.display = 'inline-block';
+        boxes[i].style.display = 'inline-block';    /* Se itera desde currentItem1 hasta currentItem1 + 4 y mostrando 4 elementos, ademas establece su estilo de visualización como 'inline-block */
     }
     currentItem1 += 4;
     if(currentItem1 >= boxes.length){
-        loadMoreBtn1.style.display = 'none';
+        loadMoreBtn1.style.display = 'none'; /*  Se verifica si currentItem1 es mayor o igual a los elementos en boxes. Si es así, se oculta el botón loadMoreBtn1 estableciendo su visualización en 'none'.  */
     }
 }
 
